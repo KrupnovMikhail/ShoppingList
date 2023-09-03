b0ac3511fd386e9da049f4eea2145176fd82bf82
Mikhail Krupnov <krupnovmn@gmail.com>
2023-09-04 00:44:27 +0300

    add domain layer

---
+++ app/src/main/java/com/krupnov/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.krupnov.shoppinglist.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
--- app/src/main/java/com/krupnov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/krupnov/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.krupnov.shoppinglist
+package com.krupnov.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krupnov.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
