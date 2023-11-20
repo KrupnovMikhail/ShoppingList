0965d4ad55c39e141bcd9d2386aa58bc758db298
Mikhail Krupnov <krupnovmn@gmail.com>
2023-11-21 02:00:26 +0300

    add dagger

--- app/src/main/java/com/krupnov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/krupnov/shoppinglist/presentation/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.krupnov.shoppinglist.R
 import com.krupnov.shoppinglist.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
 
@@ -20,13 +21,22 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
     private lateinit var shopListAdapter: ShopListAdapter
     private lateinit var binding: ActivityMainBinding
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val component by lazy {
+        (application as ShopApplication).component
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRecyclerView()
         viewModel =
-            ViewModelProvider(this).get(MainViewModel::class.java)/*[MainViewModel::class.java]*/
+            ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)/*[MainViewModel::class.java]*/
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
         }
